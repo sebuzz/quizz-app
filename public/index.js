64576723780a9ca95ef8ce28897f28e2f7defcea
brezn <br3zn@users.noreply.github.com>
2021-12-16 12:51:06 +0100

    refactored showAnswer script

--- public/index.js
+++ public/index.js
@@ -1,21 +1,32 @@
 // refactored bookmark script
 const cards = document.querySelectorAll('.js-cards');
-cards.forEach((card, i) => {
+
+
+cards.forEach((card) => {
+	// bookmark
 	const bookmark = card.querySelector('.js-bookmark');
-	const bookmarked = 'js-bookmarked';
+	const bookmarked = 'Card-bookmarkIcon--bookmarked';
 	bookmark.addEventListener('click', ev => {
 		bookmark.classList.toggle(bookmarked);
 	});
-});
 
-
-const buttons = document.querySelectorAll(".ShowAnswer");
-const answer = document.querySelectorAll(".Answer");
-const answerShowing = "Answer--showing";
-
-buttons.forEach((button, index) => {
-	const buttonSelected = "ShowAnswer";
-	button.addEventListener("click", ev => {
-		answer[index].classList.toggle(answerShowing);
+	// show answer button
+	const button = card.querySelector('.js-button');
+	const answer = card.querySelector('.js-answer');
+	const answerShowing = "Answer--showing";
+	button.addEventListener('click', ev => {
+		answer.classList.toggle(answerShowing);
 	});
 });
+
+
+// const buttons = document.querySelectorAll(".ShowAnswer");
+// const answer = document.querySelectorAll(".Answer");
+// const answerShowing = "Answer--showing";
+
+// buttons.forEach((button, index) => {
+// 	const buttonSelected = "ShowAnswer";
+// 	button.addEventListener("click", ev => {
+// 		answer[index].classList.toggle(answerShowing);
+// 	});
+// });
