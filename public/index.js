61650ecdbae2c9b1ad352f6ad369798de84a8421
brezn <br3zn@users.noreply.github.com>
2021-12-16 12:32:07 +0100

    removed legacy script & dev comments

--- public/index.js
+++ public/index.js
@@ -1,30 +1,6 @@
-// const button = document.querySelector(".ShowAnswer");
-
-// const answer = document.querySelector(".Answer");
-
-// const buttonSelected = "ShowAnswer";
-
-// const answerShowing = "Answer--showing";
-
-// button.addEventListener("click", ev => {
-// 	answer.classList.toggle("Answer--showing");
-// });
-
-// const bookmarks = document.querySelectorAll(".Bookmark");
-
-// bookmarks.forEach((bookmark, index) => {
-// 	const bookmarkIcon = bookmark.querySelector(".Card-bookmarkIcon");
-// 	const bookmarked = "Card-bookmarkIcon--bookmarked";
-
-// 	bookmark.addEventListener("click", ev => {
-// 		bookmarkIcon.classList.toggle(bookmarked);
-// 	});
-// });
-
 // refactored bookmark script
 const cards = document.querySelectorAll('.js-cards');
 cards.forEach((card, i) => {
-	console.log(card, i);
 	const bookmark = card.querySelector('.js-bookmark');
 	const bookmarked = 'js-bookmarked';
 	bookmark.addEventListener('click', ev => {
