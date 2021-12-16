9edf529eac7e134ac605d49173039da6ae018c2f
Sebastian Künne <sebuzz@mac.com>
2021-12-16 11:55:41 +0100

    minor changes

--- public/index.js
+++ public/index.js
@@ -1,24 +1,33 @@
-const button = document.querySelector(".ShowAnswer");
+// const button = document.querySelector(".ShowAnswer");
 
-const answer = document.querySelector(".Answer");
+// const answer = document.querySelector(".Answer");
 
-const buttonSelected = "ShowAnswer";
+// const buttonSelected = "ShowAnswer";
 
-const answerShowing = "Answer--showing";
+// const answerShowing = "Answer--showing";
 
-button.addEventListener("click", ev => {
-	answer.classList.toggle("Answer--showing");
-});
+// button.addEventListener("click", ev => {
+// 	answer.classList.toggle("Answer--showing");
+// });
 
 const bookmarks = document.querySelectorAll(".Bookmark");
-console.log(bookmarks);
 
 bookmarks.forEach((bookmark, index) => {
-	// const bookmark = document.querySelector(".Bookmark");
 	const bookmarkIcon = bookmark.querySelector(".Card-bookmarkIcon");
 	const bookmarked = "Card-bookmarkIcon--bookmarked";
 
 	bookmark.addEventListener("click", ev => {
 		bookmarkIcon.classList.toggle(bookmarked);
+	});
+});
+
+const buttons = document.querySelectorAll(".ShowAnswer");
+const answer = document.querySelectorAll(".Answer");
+const answerShowing = "Answer--showing";
+
+buttons.forEach((button, index) => {
+	const buttonSelected = "ShowAnswer";
+	button.addEventListener("click", ev => {
+		answer[index].classList.toggle(answerShowing);
 	});
 });
