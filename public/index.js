f179db1d7535c5d5adb1e12869fb736ca5e274a8
Konrad Scherb <konrad.scherb@gmx.de>
2021-12-17 16:25:36 +0100

    Add localStorage storing-feature

--- public/index.js
+++ public/index.js
@@ -39,7 +39,7 @@ const cards = document.querySelectorAll(".js-card");
 const bookmarked = "Card-bookmarkIcon--bookmarked";
 const answerShowing = "Answer--showing";
 
-console.log([cards]);
+// console.log([cards]);
 
 // Adjust answer field height based on its content
 const answerField = document.querySelectorAll(".js-answer");
@@ -49,17 +49,52 @@ window.addEventListener("load", event => {
 		const height = el.scrollHeight;
 		el.style.setProperty("--max-height", height + "px");
 
-		console.log(height);
-		console.log();
+		// console.log(height);
+		// console.log();
 	});
 });
 
 cards.forEach(card => {
+	// Get checkbox for each card
+	const checkbox = card.querySelector(".js-bookmark");
+
+	const cardId = card.id;
+
+	// Check in localStorage If cardObj exists by cardObj id
+	// If True
+	// Get bookmarked value
+	// If bookmarked value true/false
+	// Set checkbox.checked = bookmarkStatus
+	// If False
+	// Create cardObj
+
+	console.log("Object created");
+	console.log(localStorage.getItem(`${cardId}`));
+	// Write cardObj to localStorage
+
+	console.log(checkbox);
+	console.log(cardId);
+
+	// const user = { name: 'Alex', job: 'Software Engineer' };
+	// localStorage.setItem('user', user);
+	// console.log(localStorage.getItem('user')); // [object Object]
+
 	//const bookmark = card.querySelector(".js-bookmark");
 	const bookmarkIcon = card.querySelector(".js-bookmarkIcon");
-	// Toggle Bookmark selection
-	bookmarkIcon.addEventListener("click", ev => {
-		bookmarkIcon.classList.toggle(bookmarked);
+	// // Toggle Bookmark selection
+	// 	bookmarkIcon.addEventListener("click", ev => {
+
+	// });
+
+	checkbox.addEventListener("change", event => {
+		const bookmarkStatus = event.target.checked;
+
+		console.log("=>", bookmarkStatus);
+		bookmarkIcon.classList.toggle(bookmarked, bookmarkStatus);
+		const cardObj = { bookmarked: bookmarkStatus };
+		localStorage.setItem(cardId, JSON.stringify(cardObj));
+
+		//event.target.checked; = getItem
 	});
 
 	//Answers
